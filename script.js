9cfcb18b425de5e1dd1ef45cac73def8ea3e0a85
Gabriel Ribas <gabrielr.7@hotmail.com>
2022-01-10 15:13:19 -0300

    Criando variaveis para cronometro e exibindo cronometro na pagina

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+var horas        = 0
+var minutos      = 0
+var segundos     = 0
+var milisegundos = 0
+
+var numeros = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09",
+               "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+               "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", 
+               "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", 
+               "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", 
+               "50", "51", "52", "53", "54", "55", "56", "57", "58", "59"]
+
+var tempo = numeros[horas] + ":" + numeros[minutos] + ":" + numeros[segundos] + ":" + numeros[milisegundos]
+
+// ***** Botoes *****
+
 var botaoIniciar = document.createElement("button")
 botaoIniciar.id = "botaoIniciar"
 botaoIniciar.textContent = "Iniciar"
@@ -22,18 +38,21 @@ botaoZerar.onclick = function() {
     zerarTempo()
 }
 
-function paginaCronometro() {
-    document.getElementById("botoes").appendChild(botaoIniciar)
-    document.getElementById("botoes").appendChild(botaoZerar)
-}
+// ***** Funcoes *****
 
-paginaCronometro()
+function paginaCronometro() {
+    document.getElementById("valor").innerHTML = tempo
+    document.getElementById("botoes").appendChild(botaoIniciar)
+    document.getElementById("botoes").appendChild(botaoZerar)       
+}
 
 function iniciarTempo() {
     document.getElementById("botaoIniciar").remove()
     document.getElementById("botaoZerar").remove()
     document.getElementById("botoes").appendChild(botaoParar)
     document.getElementById("botoes").appendChild(botaoZerar)
+
+
 }
 
 function pararTempo() {
@@ -46,3 +65,5 @@ function pararTempo() {
 function zerarTempo() {
     
 }
+
+paginaCronometro()
